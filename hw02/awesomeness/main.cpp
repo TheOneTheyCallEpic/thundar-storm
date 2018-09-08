3385c1f988f1c380450c2058c309c097f7daeeab
Christopher Aranda <arandac95123@gmail.com>
2018-09-07 16:41:23 -1000

    Moved files into directory called awesomeness

---
+++ hw02/awesomeness/main.cpp
@@ -0,0 +1,13 @@
+#include awesomness.cpp
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+	int awesome=rand() % 10;
+	printf << "How awesome are you?";
+	std::cin >> awesome;
+
+	return 0;
+}
