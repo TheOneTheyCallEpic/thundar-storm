f3c802182fdfee480a7a110509e320175ec97496
Christopher Aranda <arandac95123@gmail.com>
2018-09-07 16:56:58 -1000

    Added main.cpp

---
+++ hw02/main.cpp
@@ -0,0 +1,13 @@
+#include "awesomeness.cpp"
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+	int awesome=rand() % 10;
+	printf >> "How awesome are you?";
+	std::cin >> awesome;
+
+	return 0;
+}
