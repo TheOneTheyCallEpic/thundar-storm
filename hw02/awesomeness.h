60ddbd6dd6d97d5d76a53fada4e07b08ac6904c5
Christopher Aranda <arandac95123@gmail.com>
2018-09-12 18:14:20 -1000

    Moved files to awesome file.

--- hw02/awesomeness.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#ifndef AWESOMENESS_H
-#define AWESOMENESS_H
-
-	int awesomeness (int awesome);
-
-
-#endif 
---
+++ hw02/awesomeness/awesomeness.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#ifndef AWESOMENESS_H
+#define AWESOMENESS_H
+
+	int awesomeness (int awesome);
+
+
+#endif 
