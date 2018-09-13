d546718d4675e05fb689d8849c3fb1f012dcb8b1
Christopher Aranda <arandac95123@gmail.com>
2018-09-12 17:46:54 -1000

    Added and re-editied files

---
+++ hw02/awesomeness.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "awesomeness.h"
+
+	int awesomeness(int awesome){
+
+		switch (awesome)
+	{
+
+		case 0:
+			printf ("Oh god, you suck man. %d",awesome);
+			break;
+		case 1:
+			printf ("Eh, you're not bad but you still suck. %d",awesome);
+			break;
+		case 2:
+			printf ("Ok, you kinda suck but not so much. %d",awesome);
+			break;		
+		case 3:
+			printf ("Sucking, but you're not bad. You can be better. %d",awesome);
+			break;
+		case 4:
+			printf ("You're below average, but you don't suck. %d",awesome);
+			break;
+		case 5:
+			printf ("You're average. %d",awesome);
+			break;
+		case 6:
+			printf ("Not too bad... %d",awesome);
+			break;
+		case 7:
+			printf ("Hey, that's pretty good. %d",awesome);
+			break;
+		case 8:
+			printf ("Woo, you're awesome! %d",awesome);
+			break;		
+		case 9:
+			printf ("Paul McCartney would be proud. %d",awesome);
+			break;
+		case 10:
+			printf ("You're Jesus. %d",awesome);
+			break;
+
+		}
+
+		return awesome;
+	}
+
+	
+
