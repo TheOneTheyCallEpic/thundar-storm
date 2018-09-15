1f6da17facf9f281805836844640dd4122014477
Christopher Aranda <arandac95123@gmail.com>
2018-09-15 13:14:58 -1000

    Just more changes...

--- hw03/Program_Run/program_run.cc
+++ hw03/Program_Run/program_run.cc
@@ -5,11 +5,15 @@
 
 int program_run(int n){
 
-FILE* fp = fopen(“filename”, “r”);
+FILE* fp = fopen("numfile.txt", “r”); //Opens file
+EOF=false;
 int n;
+
 while(fscanf(fp, “%d”, value) != EOF) {
 
 printf(“%d\n”, value); }
 fclose(fp);
+
+return 0;
 
 }
